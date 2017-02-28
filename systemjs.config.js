35b080e724d7d792bc8f6ef8472eaf0085563b85
harshal-comcast <harshal_shah@comcast.com>
2017-02-27 23:58:52 -0500

    Skeleton ready (#5)
    * Skeleton ready

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,15 +1,14 @@
 (function(global){
     var map = {
         app: 'app',
-        '@angular/core': 'npm@angular/core/bundles/core.omd.js',
-        '@angular/common': 'npm@angular/core/bundles/common.omd.js',
-        '@angular/compiler': 'npm@angular/core/bundles/compiler.omd.js',
-        '@angular/platform-browser': 'npm@angular/core/bundles/platform-browser.omd.js',
-        '@angular/platform-browser-dynamic': 'npm@angular/core/bundles/platform-browser-dynamic.omd.js',
-        '@angular/http': 'npm@angular/core/bundles/http.omd.js',
-        '@angular/router': 'npm@angular/core/bundles/router.omd.js',
-        '@angular/forms': 'npm@angular/core/bundles/forms.omd.js',
-
+        '@angular/core': 'npm@angular/core/bundles/core.umd.min.js',
+        '@angular/common': 'npm@angular/common/bundles/common.umd.min.js',
+        '@angular/compiler': 'npm@angular/compiler/bundles/compiler.umd.min.js',
+        '@angular/platform-browser': 'npm@angular/platform-browser/bundles/platform-browser.umd.min.js',
+        '@angular/platform-browser-dynamic': 'npm@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.min.js',
+        '@angular/http': 'npm@angular/http/bundles/http.umd.min.js',
+        '@angular/router': 'npm@angular/router/bundles/router.umd.min.js',
+        '@angular/forms': 'npm@angular/forms/bundles/forms.umd.min.js',
         'rxjs': 'npmrxjs',
         'angular-in-memory-web-api': 'npmangular-in-memory-web-api'
     };
@@ -19,7 +18,7 @@
         'rxjs': { defaultExtension: 'js' },
         'angular2-in-memory-web-api': { main:'./index.js' , defaultExtension: 'js' },
     }; 
-
+    
     System.config(
         {
             paths: {
